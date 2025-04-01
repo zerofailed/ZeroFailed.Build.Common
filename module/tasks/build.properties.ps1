9fb5c5535d85381aa14536b0fdb6d848fc643bfc
James Dawson <james.dawson@endjin.com>
2025-04-01 14:28:58 +0100

    Fix issue with the environment variable overrides not working on Linux, due to case-sensitivity issues.

--- module/tasks/build.properties.ps1
+++ module/tasks/build.properties.ps1
@@ -3,14 +3,14 @@
 # </copyright>
 
 # Synopsis: When true, the build process will clean intermediate & output directories before starting the build.
-$CleanBuild = property ZF_CleanBuild $false
+$CleanBuild = property ZF_BUILD_CLEAN $false
 
 # Synopsis: The directory path where packages produced by the build will be placed.
-$PackagesDir ??= property ZF_PackagesDir '_packages'
+$PackagesDir ??= property ZF_BUILD_PACKAGES_DIR '_packages'
 
 # Synopsis: The directory path where code coverage files produced by the build will be placed.
-$CoverageDir ??= property ZF_CoverageDir '_codeCoverage'
+$CoverageDir ??= property ZF_BUILD_COVERAGE_DIR '_codeCoverage'
 $CoverageDir = [IO.Path]::IsPathRooted($CoverageDir) ? $CoverageDir : (Join-Path $here $CoverageDir)
 
 # Synopsis: When true (and running on a DevOps agent), the build process will send phase completion messages to the agent.
-$SendPhaseCompletionMessagesToCICDServer = property ZF_SendPhaseCompletionMessagesToCICDServer $true
+$SendPhaseCompletionMessagesToCICDServer = property ZF_BUILD_PHASE_COMPLETION_MESSAGES $true
