98ec448d4d52c5dcc5ee4f9b8b0a46748ed935b3
James Dawson <james.dawson@endjin.com>
2025-07-25 14:24:36 +0100

    Fix typo in .psm1 file

--- module/ZeroFailed.Build.Common.psm1
+++ module/ZeroFailed.Build.Common.psm1
@@ -1,4 +1,4 @@
-# <copyright file="ZeroFailed.Build.Common.psd1" company="Endjin Limited">
+# <copyright file="ZeroFailed.Build.Common.psm1" company="Endjin Limited">
 # Copyright (c) Endjin Limited. All rights reserved.
 # </copyright>
 
